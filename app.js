46a6154dbc9fe116a62b4d28e003a903c1362178
Ulf Mandorff <ulf.mandorff@utb.ecutbildning.se>
2021-11-29 20:05:47 +0100

    7 klar

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ function taskFive() {
     } else {
       eatable.push(element);
     }
-    elem.innerHTML = "Ätligt: " + eatable + "\nSkräp: " + trash;
+    elem.innerHTML = "Ätligt: " + eatable + "<br>Skräp: " + trash;
   });
 }
 
@@ -126,7 +126,32 @@ const overThirty = [];
 const underThirty = [];
 
 function taskSix() {
-  alert("Replace this alert with a solution");
+  for (const [key, value] of Object.entries(persons)) {
+    if (value.married) {
+      married.push(persons[key].name);
+    } else {
+      notMarried.push(persons[key].name);
+    }
+    if (value.age > 30) {
+      overThirty.push(persons[key].name);
+    } else {
+      underThirty.push(persons[key].name);
+    }
+  }
+  let elem = document.getElementById("answer-six");
+  elem.innerHTML =
+    "<p><b>Över 30: </b>" +
+    overThirty +
+    "</p>" +
+    "<p><b>Under 30: </b>" +
+    underThirty +
+    "</p>" +
+    "<p><b>Gift: </b>" +
+    married +
+    "</p>" +
+    "<p><b>Ogift: </b>" +
+    notMarried +
+    "</p>";
 }
 
 /*
@@ -137,9 +162,17 @@ ut texten på en ny rad i diven "answer-seven"
 
 const addMeSomeLineBreaks =
   "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
-
+let out="";
 function taskSeven() {
-  alert("Replace this alert with a solution");
+  out = "";
+  for(let i=0; i<addMeSomeLineBreaks.length; i++){
+    out += addMeSomeLineBreaks[i];
+    if(addMeSomeLineBreaks[i] == ","){
+      out +=  " <br>";
+    }
+  }
+  let elem = document.getElementById("answer-seven");
+  elem.innerHTML = out;
 }
 
 /*
@@ -149,7 +182,7 @@ efter att tre sekunder har gått ska diven visas som vanligt igen
 */
 
 function taskEight() {
-  alert("Replace this alert with a solution");
+  
 }
 
 /*
